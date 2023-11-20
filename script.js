c0e2b9f9c53e952241d28f44d986cdc2ea2978cc
Annikamekk <144127346+Annikamekk@users.noreply.github.com>
2023-11-21 00:01:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var originalImage = document.querySelector('main > img');
+
+  function changeImage() {
+    originalImage.src = 'Img/Friends.jpg';
+  }
+
+  function restoreImage() {
+    originalImage.src = 'Img/CVfotocropped.png';
+  }
+
+  
+  console.log('hoi')
